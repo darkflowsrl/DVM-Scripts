e5632ad8a2b2e612bf36d55c39f84442c1ed2014
Facundo Segarra <segarrafacundoroman@gmail.com>
2024-09-21 08:18:12 -0300

    Actualizando

--- scripts/download-front.sh
+++ scripts/download-front.sh
@@ -1,27 +1,20 @@
-#!/bin/bash
+#!/usr/bin/csh
 
-if [ $# -lt 4 ] ;then
-    echo "Usage: <github token> <org/repo> <filename> <version or 'latest'>"
+if [ $# -lt 1 ] ;then
+    echo "Usage: <version or 'latest'>"
     exit 1
 fi
 
-TOKEN="$1"
-REPO="$2"
-FILE="$3"      # the name of your release asset file, e.g. build.tar.gz
-VERSION="$4"                       # tag name or the word "latest"
+VERSION="$1" # tag name or the word "latest"
+REPO=darkflowsrl/DVM-front
+FILE=frontend.AppImage
 GITHUB_API_ENDPOINT="api.github.com"
-
 alias errcho='>&2 echo'
 
-# dpkg --configure -a
-# apt update && apt upgrade
-
-# apt install -y jq curl unrar
-
 function gh_curl() {
-  curl -sL -H "Authorization: token $TOKEN" \
-       -H "Accept: application/vnd.github.v3.raw" \
-       $@
+  curl \
+    -H "Accept: application/vnd.github.v3.raw" \
+    $@
 }
 
 if [ "$VERSION" = "latest" ]; then
@@ -32,17 +25,21 @@ else
 fi
 
 ASSET_ID=`gh_curl https://$GITHUB_API_ENDPOINT/repos/$REPO/releases | jq "$PARSER"`
+echo "ASSET_ID: $ASSET_ID"
 if [ "$ASSET_ID" = "null" ]; then
   errcho "ERROR: version not found $VERSION"
   exit 1
 fi
 
-m=$(curl -sL --header "Authorization: token $TOKEN" --header 'Accept: application/octet-stream' https://$TOKEN:@$GITHUB_API_ENDPOINT/repos/$REPO/releases/assets/$ASSET_ID > /root/frontend/$FILE 2>&1)
-#m=$(curl -sL --header "Authorization: token $TOKEN" --header 'Accept: application/octet-stream' https://$TOKEN:@$GITHUB_API_ENDPOINT/repos/$REPO/releases/assets/$ASSET_ID > $FILE 2>&1)
+m=$(curl -sL --header 'Accept: application/octet-stream' https://$GITHUB_API_ENDPOINT/repos/$REPO/releases/assets/$ASSET_ID > /root/frontend/$FILE.temp 2>&1)
 if [ $? -ne 0 ] ; then
   echo "Error: ""$m"
-  exit 1  
+  exit 1
 fi
+
+yes | cp -f "/root/frontend/$FILE.temp" "/root/frontend/$FILE"
+
+rm "/root/frontend/$FILE.temp"
 
 chmod +x "/root/frontend/$FILE"
 
@@ -60,23 +57,17 @@ fi
 
 ASSET_ID=`gh_curl https://$GITHUB_API_ENDPOINT/repos/$REPO/releases | jq "$PARSER"`
 if [ "$ASSET_ID" != "null" ]; then
-  m=$(curl -sL --header "Authorization: token $TOKEN" --header 'Accept: application/octet-stream' https://$TOKEN:@$GITHUB_API_ENDPOINT/repos/$REPO/releases/assets/$ASSET_ID > /root/frontend/data.rar 2>&1)
+  m=$(curl -sL --header 'Accept: application/octet-stream' https://$GITHUB_API_ENDPOINT/repos/$REPO/releases/assets/$ASSET_ID > /root/data.rar 2>&1)
   if [ $? -ne 0 ] ; then
     echo "Error: ""$m"
     exit 1
   fi
 
-  # BACKUP_FILES_CONFIG_DIR="/root/backup_files_config_$(date +%Y%m%d_%H%M%S)"
-  # mkdir -p "$BACKUP_FILES_CONFIG_DIR" 
-  # cp -r /root/dvm-app-front/* "$BACKUP_FILES_CONFIG_DIR" 
-  
-  # mkdir -p "/root/dvm-app-front"
-  # mkdir -p "dvm-app-front"
-  unrar x -y -r /root/frontend/data.rar /root/frontend
-  #unrar x -y -r data.rar dvm-app-front
+  mkdir -p "/root/frontend/data"
+  unrar x -y -r /root/data.rar /root/frontend/data
+  rm /root/data.rar
   chmod -R 777 "/root/frontend"
-  #chmod -R 777 "dvm-app-front"
   echo "Agregando archivos de configuración"
 fi
 
-  
+
